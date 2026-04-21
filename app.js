a087b23be06ddee4ee7db80e7762ebd1d682de01
Muhammad hashim <mh4165209@gmail.com>
2026-04-21 09:43:10 -0700

    array practice and math.round& floor & ceil

---
+++ app.js
@@ -0,0 +1,9 @@
+// var months=["pop", false, 20.2232,"bahi",true,"test"];
+// console.log(typeof(months[months.length -5]));
+// var a="this is an apple";
+// a=a.slice(5,7);
+// console.log(a);
+// var b=9.1;
+// b=Math.ceil(b);
+// console.log(b);
+
